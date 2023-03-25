87c2ad27cc6cdc8efae79da3e80e9f2d01fc99e7
rare-wolf <gabeduncan018@gmail.com>
2023-03-25 17:54:29 -0400

    visualize one day of snow data

--- plot_snow_data.py
+++ plot_snow_data.py
@@ -2,21 +2,22 @@ import pandas as pd
 import plotly.graph_objects as go
 
 # Load data from CSV file
-data = pd.read_csv('ca_snow_data.csv')
+data = pd.read_csv('data_final.csv')
+
+# Filter data to include only the desired time point
+time_point = 1675652400
+data = data[data['time_start'] == time_point]
+
+print(data.head(5))
 
 # Create 3D surface plot
-fig = go.Figure(data=[go.Surface(x=data['longitude'], y=data['latitude'], z=data['sde'], colorscale='Blues')])
+fig = go.Figure(data=[go.Scatter3d(x=data['longitude'], y=data['latitude'], z=data['sde'], 
+                                   mode='markers', marker=dict(color=data['sde'], colorscale='inferno'))])
 
-# Add slider for time
-fig.update_layout(
-    sliders=[{
-        'currentvalue': {'prefix': 'Time: '},
-        'steps': [
-            {'label': str(time), 'method': 'update', 'args': 
-                [{'visible': [time == t for t in data['time_start']]}]
-            } for time in data['time_start'].unique()
-        ]
-    }]
-)
+# Set axis ranges
+fig.update_layout(scene=dict(xaxis=dict(range=[data['longitude'].min(), data['longitude'].max()]),
+                             yaxis=dict(range=[data['latitude'].min(), data['latitude'].max()]),
+                             zaxis=dict(range=[data['sde'].min(), data['sde'].max()])))
+
 
 fig.show()
