b3b361c1d31c8c599672dcb11a80c6483433fa55
rare-wolf <gabeduncan018@gmail.com>
2023-03-27 21:30:20 -0400

    new color scale

--- data_extractor.py
+++ data_extractor.py
@@ -17,7 +17,7 @@ class DataExtractor:
             host=self.config['HOST'], 
             database='weather'
         )
-        with open(f'state_wkt_files/{self.state}.wkt', 'r') as wkt_file:
+        with open(f'states/{self.state}.wkt', 'r') as wkt_file:
             self.wkt_string = wkt_file.read()
         self.state_polygon = wkt.loads(self.wkt_string)
         # Create a bounding box around the state polygon
@@ -27,11 +27,12 @@ class DataExtractor:
             FROM weather
             WHERE time_start > {self.start_time}
             AND MOD(time_start - {self.start_time}, {self.increment}) = 0
-            AND ST_Contains(
-                ST_GeomFromText('{self.bbox.wkt}'),
-                POINT(longitude, latitude)
-            )
+            AND latitude > {self.bbox.bounds[1]}
+            AND latitude < {self.bbox.bounds[3]}
+            AND longitude > {self.bbox.bounds[0]}
+            AND longitude < {self.bbox.bounds[2]}
         """
+
     
     def in_state(self, lng, lat):
         point = Point(lng, lat)
@@ -47,7 +48,7 @@ class DataExtractor:
         with open(f'snow_csv_data/{self.state}_snow_data.csv', 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(['time_start', 'latitude', 'longitude', 'sde'])
-            batch_size = 10000
+            batch_size = 100
             while True:
                 results = cursor.fetchmany(batch_size)
                 if not results:
@@ -57,5 +58,10 @@ class DataExtractor:
                     if self.in_state(row[2], row[1]): # lng, lat
                         writer.writerow(row)
 
-extractor = DataExtractor('california')
-extractor.extract_data()
+states = ['alabama', 'arkansas', 'connecticut', 'delaware', 'florida', 'georgia', 'illinois', 'indiana', 'iowa', 'kansas', 'kentucky', 'louisiana', 'maine', 'maryland', 'michigan', 'minnesota', 'mississippi', 'missouri', 'nebraska', 'new_hampshire', 'new_jersey', 'new_york', 'north_carolina', 'north_dakota', 'ohio', 'oklahoma', 'pennsylvania', 'rhode_island', 'south_carolina', 'south_dakota', 'tennessee', 'texas', 'vermont', 'virginia', 'west_virginia', 'wisconsin']
+
+
+for state in states:
+    print(state)
+    extractor = DataExtractor(state)
+    extractor.extract_data()
--- plot_generator.py
+++ plot_generator.py
@@ -7,59 +7,88 @@ import matplotlib.colors as mcolors
 from mpl_toolkits.mplot3d import Axes3D
 
 class PlotGenerator:
-    def __init__(self, states):
+    def __init__(self, type, states):
+        self.type = type
         self.states = states
         self.data = pd.concat([pd.read_csv(f'snow_csv_data/{state}_snow_data.csv') for state in states])
         self.unique_times = sorted(self.data['time_start'].unique())
 
-        self.fig = plt.figure(figsize=(12, 9))
-        self.fig.subplots_adjust(top=0.85, bottom=0.15)
-        self.ax = self.fig.add_subplot(111, projection='3d')
-        self.ax.set_box_aspect([1, 1, None])
+        if self.type == '3d':
+            self.fig = plt.figure(figsize=(12, 9))
+            self.fig.subplots_adjust(top=0.85, bottom=0.15)
+            self.ax = self.fig.add_subplot(111, projection='3d')
+            self.ax.set_box_aspect([1, 1, None])
+        
+        elif self.type == 'contour':
+            self.fig, self.ax = plt.subplots(figsize=(12, 9))
+            self.ax.set_aspect('equal')
+
         # Create colorbar
-        self.colorscale = [
+        self.colorscale = [    
             (0, (0, 0, 0)),
-            (0.01, (0, 1, 1)),
-            (0.2, (0, 0, 1)),
-            (0.4, (130/255, 0, 255/255)),
-            (0.6, (1, 0, 1)),
-            (0.8, (1, 1, 0)),
+            (0.008, (0, 1, 1)),
+            (0.12, (0, 0, 1)),
+            (0.24, (130/255, 0, 255/255)),
+            (0.4, (1, 0, 1)),
+            (0.5, (1, 0, 0)),
+            (0.72, (1, 1, 0)),
             (1, (1, 1, 1))
         ]
         self.cmap = mcolors.LinearSegmentedColormap.from_list('custom', self.colorscale)
         self.sde_max = np.max(self.data['sde']) * 3.28084
         sm = mpl.cm.ScalarMappable(cmap=self.cmap, norm=mpl.colors.Normalize(vmin=0, vmax=self.sde_max))
         sm.set_array([])
-        cbar = self.fig.colorbar(sm, shrink=0.5, aspect=10, pad=-0.17)
+        cbar = self.fig.colorbar(sm, shrink=0.5, aspect=10, pad=-0.15)
         cbar.ax.set_ylabel('Snow Depth (ft)')
         cbar.ax.yaxis.set_ticks_position('left')
         cbar.ax.yaxis.set_label_position('left')
         cbar.ax.tick_params(length=0)
         cbar.ax.set_position([0.75, 0.1, 0.03, 0.9])
 
-    def update(self, i):
-        print(f'{i} of {len(self.unique_times)}')
+    def update_3d(self, i):
         time = self.unique_times[i]
         subset = self.data[self.data['time_start'] == time]
         x, y, z = subset['longitude'], subset['latitude'], subset['sde'] * 3.28084
         dz = z / self.sde_max
         self.ax.clear()
-        self.ax.bar3d(x, y, np.zeros(len(z)), dx=0.16, dy=0.16, dz=dz, color=self.cmap(dz), edgecolor=None)
-        self.ax.set_box_aspect([0.6, 1, 1])  # Set aspect ratio of x, y, z axes
-        self.ax.set_title(pd.to_datetime(time, unit="s").strftime("%m/%d/%Y"), pad=-20, y=0.96)
+        self.ax.bar3d(x, y, np.zeros(len(z)), dx=0.16, dy=0.16, dz=z, color=self.cmap(dz), edgecolor=None)
+        self.ax.set_box_aspect([0.9, 1, 1])  # Set aspect ratio of x, y, z axes
+        self.ax.set_title(pd.to_datetime(time, unit="s").strftime("%m/%d/%Y"), pad=-20, y=0.9)
         self.fig.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)
         self.ax.axis('off')
-        self.ax.view_init(elev=80, azim=-90)
+        self.ax.view_init(elev=60, azim=-90)
         self.ax.set_zlim(0, self.sde_max)
+    
+    def update_contour(self, i):
+        time = self.unique_times[i]
+        subset = self.data[self.data['time_start'] == time]
+        x, y, z = subset['longitude'], subset['latitude'], subset['sde'] * 3.28084
+        dz = z / self.sde_max
+        self.ax.clear()
+        contour = self.ax.tricontourf(x, y, z, cmap=self.cmap)
+        self.ax.set_aspect('equal')
+        self.ax.set_title(pd.to_datetime(time, unit="s").strftime("%m/%d/%Y"), pad=-20, y=0.9)
+        self.fig.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)
+        self.ax.set_xlabel('Longitude')
+        self.ax.set_ylabel('Latitude')
+        self.fig.colorbar(contour, ax=self.ax)
 
     def generate_plot(self):
-        # self.update(10)
-        # plt.show()
+        if self.type == '3d':
+            self.update_3d(0)
+            plt.show()
 
-        ani = animation.FuncAnimation(self.fig, self.update, frames=len(self.unique_times))
-        ani.save(f"snow_depth_gifs/{'_'.join(self.states)}_snow_depth.gif", writer='pillow')
+            # ani = animation.FuncAnimation(self.fig, self.update_3d, frames=len(self.unique_times))
+            # ani.save(f"snow_depth_gifs/{'_'.join(self.states)}_snow_depth_3d.gif", writer='pillow')
+        
+        if self.type == 'contour':
+            self.update_contour(140)
+            plt.show()
+
+            # ani = animation.FuncAnimation(self.fig, self.update_contour, frames=len(self.unique_times))
+            # ani.save(f"snow_depth_gifs/{'_'.join(self.states)}_snow_depth_contour.gif", writer='pillow')
 
 
 
-plot = PlotGenerator(['oregon', 'washington', 'california'])
+plot = PlotGenerator('3d', ['iowa', 'kansas', 'missouri', 'nebraska', 'colorado', 'wyoming'])
 plot.generate_plot()
